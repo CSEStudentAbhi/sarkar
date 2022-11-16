a6afd26c82701bdccf44cdf3a0c3a29518258198
Abhishek Ambi <109055711+CSEStudentAbhi@users.noreply.github.com>
2022-11-15 19:00:47 -1200

    Update a.py

--- a.py
+++ a.py
@@ -1,49 +1,15 @@
-# importing modules
-from pywebio.input import *
-from pywebio.output import *
+# Python3 code to demonstrate working of
+# Alphabets Frequency in String
+# Using isalpha() + len()
 
-# classify person
-class calculation:
+# initializing string
+test_str = 'geeksforgeeks !!$ is best 4 all Geeks 10-0'
 
-	# defining method
-	def BMIcalculator(Height, Mass):
+# printing original string
+print("The original string is : " + str(test_str))
 
-		for t1, t2 in [(16, 'severely underweight'),
-					(18.5, 'underweight'),
-					(25, 'normal'),
-					(30, 'overweight'),
-					(35, 'moderately obese'),
-					(float('inf'), 'severely obese')]:
-			if BMI <= t1:
-				put_text('Your BMI is', BMI, 'and the person is :', t2)
-				break
+# isalpha() to computation of Alphabets
+res = len([ele for ele in test_str if ele.isalpha()])
 
-# classify and compute BMI
-class calculation:
-
-	# defining method
-	def BMIcalculator(self, Height, Mass):
-
-		# compute BMI
-		BMI = (Mass)/(Height*Height)
-
-		# classify
-		for t1, t2 in [(16, 'severely underweight'),
-					(18.5, 'underweight'),
-					(25, 'normal'), (30, 'overweight'),
-					(35, 'moderately obese'),
-					(float('inf'), 'severely obese')]:
-
-			if BMI <= t1:
-				put_text('Your BMI is', BMI, 'and the person is :', t2)
-				break
-
-
-# height input
-Height = input("Please enter height in meters(m)", type=FLOAT)
-
-# Mass input
-Mass = input("Please enter Mass/Weight in Kilograms(Kg)", type=FLOAT)
-
-obj = calculation()
-obj.BMIcalculator(Height, Mass)
+# printing result
+print("Count of Alphabets : " + str(res))
